54f098d54da1d50b1ec300d14127bca63d2b18ce
Joel Pagliuca <Joel.Pagliuca@cba.com.au>
2017-08-20 22:37:56 +1000

    more tests

--- tests.py
+++ tests.py
@@ -5,16 +5,24 @@ from unittest import TestCase
 
 from main import *
 
-class AllTests(TestCase):
+class CGTestCase(TestCase):
 
 	def setUp(self):
 		self.card1 = Card(1, "blue")
 		self.card2 = Card(2, "blue")
-		self.card_list = [self.card1, self.card2]
-		self.deck = Deck("test", self.card_list)
+		self.deck = Deck("test", [self.card1, self.card2])
+		self.pile = Pile()
+
+class AllTests(CGTestCase):
 	
 	def test_draw_card(self):
 		a_card1 = self.deck.draw_card()
 		a_card2 = self.deck.draw_card()
-		self.assertIn(a_card1, self.card_list)
-		self.assertIn(a_card2, self.card_list)
+		self.assertIn(a_card1, [self.card1, self.card2])
+		self.assertIn(a_card2, [self.card1, self.card2])
+
+class PileTests(CGTestCase):
+
+	def test_pile(self):
+		self.pile.play_card(self.card1)
+		self.assertEqual(self.card1, self.pile.top_card())
