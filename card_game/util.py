e34b3034dccea93e29112a6901061b8e60f6bdfe
Joel Pagliuca <Joel.Pagliuca@cba.com.au>
2017-08-27 00:33:31 +1000

    added coverage config to omit files from report

--- card_game/util.py
+++ card_game/util.py
@@ -5,7 +5,7 @@ import constants
 
 __all__ = ["Logger"]
 
-class Logger(object):
+class Logger(object): # pragma: no cover
 	@staticmethod
 	def debug(msg, tag=constants.DEFAULT_TAG):
 		if constants.DEBUG:
