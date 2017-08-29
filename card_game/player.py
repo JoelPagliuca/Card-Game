f194845f802814f87fb4975c144fd4dada8460e8
JoelPagliuca <pagliucajoel@gmail.com>
2017-08-29 13:22:27 +1000

    differentiating between AI and human controlled Players

--- card_game/player.py
+++ card_game/player.py
@@ -7,15 +7,19 @@ class Player(object):
 	"""
 	A Player
 	"""
-	def __init__(self, name):
+	def __init__(self, name, is_human=True):
 		"""
 		:type name: str
 		"""
 		self.name = name
 		self.hand = list()
+		self._human = is_human
 	
 	def take_card(self, card):
 		"""
 		:type card: Card
 		"""
 		self.hand.append(card)
+	
+	def is_human(self):
+		return self._human
