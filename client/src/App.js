ac83568aa193c7c981fb4774282bf707a1d18265
JoelPagliuca <pagliucajoel@gmail.com>
2017-10-15 19:04:58 +1100

    cards but better engineering

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,15 @@ import './App.css';
 import {Layer, Rect, Text, Stage} from 'react-konva';
 
 const CONSTANTS = {
-  WEBSOCKET: "ws://"+window.location.host+"/websocket/gameview"
+  WEBSOCKET: "ws://"+window.location.host+"/websocket/gameview",
+  CARD_WIDTH: 100
+}
+
+const SUITS = {
+  YELLOW: '#FC0',
+  BLUE: '#05F',
+  RED: '#F00',
+  PURPLE: '#F0C'
 }
 
 class SocketComponent extends Component {
@@ -36,23 +44,23 @@ class SocketComponent extends Component {
 }
 
 class Card extends Component {
-  
+
   render() {
     return (
-      <Layer x={10} y={10}>
+      <Layer x={this.props.x} y={this.props.y}>
         <Rect
-          width={100}
+          width={CONSTANTS.CARD_WIDTH}
           height={150}
-          fill={'#FC0'}
+          fill={this.props.suit}
           stroke={'#111'}
           strokeWidth={2}
           cornerRadius={2}
         />
         <Text
-          text={'8'}
+          text={this.props.text}
           fontSize={80}
           fill={'#111'}
-          width={100}
+          width={CONSTANTS.CARD_WIDTH}
           padding={20}
           align={'center'}
         />
@@ -60,6 +68,18 @@ class Card extends Component {
     )
   }
 }
+Card.propTypes = {
+  text: React.PropTypes.string,
+  suit: React.PropTypes.string,
+  x: React.PropTypes.number,
+  y: React.PropTypes.number
+};
+Card.defaultProps = { 
+  text: '0', 
+  suit: SUITS.YELLOW,
+  x: 10,
+  y: 10
+};
 
 class App extends Component {
   render() {
@@ -71,7 +91,10 @@ class App extends Component {
         </div>
         <SocketComponent />
         <Stage width={700} height={700}>
-          <Card />
+          <Card text={'8'} suit={SUITS.BLUE}/>
+          <Card text={'4'} suit={SUITS.PURPLE} x={CONSTANTS.CARD_WIDTH+10+10}/>
+          <Card text={'R'} suit={SUITS.YELLOW} x={2*(CONSTANTS.CARD_WIDTH+10)+10}/>
+          <Card text={'0'} suit={SUITS.RED} x={3*(CONSTANTS.CARD_WIDTH+10)+10}/>
         </Stage>
       </div>
     );
