2b36d41094ac93bc07ec693444323ac030266d27
JoelPagliuca <pagliucajoel@gmail.com>
2017-10-16 22:59:01 +1100

    forgot this file

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,25 @@
 import React, { Component } from 'react';
-import WebSocket from 'react-websocket';
 import logo from './logo.svg';
 import './App.css';
 
 import {Layer, Rect, Text, Stage} from 'react-konva';
+import PropTypes from 'prop-types';
 
-const CONSTANTS = {
+export const CONSTANTS = {
   WEBSOCKET: "ws://"+window.location.host+"/websocket/gameview",
-  CARD_WIDTH: 100
-}
+  CARD_WIDTH: 100,
+  ACTIONS: {
+    UPDATE: "UPDATE",
+    OPTION: "OPTION"
+  }
+};
 
 const SUITS = {
   YELLOW: '#FC0',
   BLUE: '#05F',
   RED: '#F00',
   PURPLE: '#F0C'
-}
-
-class SocketComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: ""
-    };
-  }
-
-  handleData(data) {
-    let result = JSON.stringify(data);
-    this.setState({data: result});
-  }
-
-  render() {
-    return (
-      <div><h3>Here is the WebSocket content:</h3>
-      {this.state.data}
-
-      <WebSocket url={CONSTANTS.WEBSOCKET}
-        onMessage={this.handleData.bind(this)} />
-      
-      </div>
-    ); 
-  }
-}
+};
 
 class Card extends Component {
 
@@ -69,16 +47,62 @@ class Card extends Component {
   }
 }
 Card.propTypes = {
-  text: React.PropTypes.string,
-  suit: React.PropTypes.string,
-  x: React.PropTypes.number,
-  y: React.PropTypes.number
+  text: PropTypes.string,
+  suit: PropTypes.string,
+  x: PropTypes.number,
+  y: PropTypes.number
 };
 Card.defaultProps = { 
   text: '0', 
   suit: SUITS.YELLOW,
   x: 10,
   y: 10
+};
+
+// FIXME: enough of this export shit
+export class GameSocketComponent extends Component {
+  constructor(props) {
+    super(props);
+    const gameSocket = new WebSocket(CONSTANTS.WEBSOCKET);
+    gameSocket.onmessage = (event) => {
+      this.handleServerMessage(JSON.parse(event.data));
+    };
+  };
+
+  handleServerMessage(data) {
+    // check what action
+    switch (data.action) {
+      case "UPDATE":
+        console.log("UPDATE");
+        break;
+      case CONSTANTS.ACTIONS.OPTION:
+        console.log("OPTIONS");
+        break;
+      default:
+        console.log("action was not an expected value");
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <div/>
+    ); 
+  }
+}
+
+class Game extends Component {
+  
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+    return (
+      <GameSocketComponent
+     />
+    );
+  };
 };
 
 class App extends Component {
@@ -89,7 +113,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <SocketComponent />
+        <Game />
         <Stage width={700} height={700}>
           <Card text={'8'} suit={SUITS.BLUE}/>
           <Card text={'4'} suit={SUITS.PURPLE} x={CONSTANTS.CARD_WIDTH+10+10}/>
