58345b7f39397bfff74c8752523573d679b2f8dc
JoelPagliuca <pagliucajoel@gmail.com>
2017-10-16 22:58:43 +1100

    now correctly checking for actions from server, also I think I unit tested, hard to tell because mock-socket is FUCKY

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App, GameSocketComponent, CONSTANTS } from "./App";
 
-it('renders without crashing', () => {
+// mock up the websocket
+import { Server } from "mock-socket";
+
+it('runs the SocketComponent?', () => {
+  const mockServer = new Server(CONSTANTS.WEBSOCKET);
+  mockServer.on('connection', (socket) => { // WHAT THE ACTUAL FUCK IS THIS socket VARIABLE FROM THE EXAMPLE?
+    mockServer.send('{"action":"UPDATE"}');
+  });
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<GameSocketComponent />, div);
+  mockServer.listeners.connection[0](); // To actually run a connection, do this
 });
