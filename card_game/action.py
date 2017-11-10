65c59985bf5ccbb6399b47f055b01b1bbb5d3da5
JoelPagliuca <joelpagliuca@hotmail.com>
2017-11-10 23:13:13 +1100

    changed ids to strings

--- card_game/action.py
+++ card_game/action.py
@@ -9,7 +9,7 @@ class Action(object):
 	TAG = "ACTION"
 	def __init__(self, card):
 		self.card = card
-		self.id = id(self)
+		self.id = str(id(self))
 	"""
 	Base class
 	TODO: abs
@@ -26,7 +26,7 @@ class Action(object):
 		Convert to JSON string
 		:rtype: dict
 		"""
-		output = { "action": self.TAG }
+		output = { "action": self.TAG, "id": self.id }
 		if self.card:
 			output["card"] = self.card.toDict()
 		return output
