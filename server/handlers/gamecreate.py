0116c59e640a5a5cb24c12ffa68e5ea48689066d
JoelPagliuca <joelpagliuca@hotmail.com>
2017-11-20 23:31:16 +1100

    serving back the game_id

--- server/handlers/gamecreate.py
+++ server/handlers/gamecreate.py
@@ -3,6 +3,7 @@
 import logging
 
 from server.controller import GameController
+from server.data import GAMES
 
 from tornado.web import RequestHandler
 
@@ -18,6 +19,11 @@ class GameCreateHandler(RequestHandler):
 		num_players = self.get_body_argument("number_players", 3)
 		logging.info("going to make a game with "+str(num_players)+" players")
 		controller = GameController(max_players=num_players)
+		GAMES[controller.id] = controller
+		create_response = {
+			"game_id": controller.id
+		}
+		self.write(create_response)
 	
 	def check_xsrf_cookie(self):
 		return True # FIXME have a cleaner interface to this handler
