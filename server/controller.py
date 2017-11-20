50778a69d1d7ae7cfd4a0622e445c89a671fc59c
JoelPagliuca <joelpagliuca@hotmail.com>
2017-11-20 23:21:14 +1100

    /gamecreate that creates a GameController

--- server/controller.py
+++ server/controller.py
@@ -1,21 +1,35 @@
 """
 """
 import uuid
+import logging
+import threading
 
+from util import WebSocketInterface
 
+from card_game.engine import GameManager
+from card_game.rules import MelbourneRules as RULES
+from card_game.data.decks import GET_UNO_DECK as GET_DECK
 
 __all__ = ["GameController"]
 
-class GameController(obkect):
+class GameController(object):
 	"""deals with the game and provides a nicer interface for the WebSocket view"""
-	def __init__(self):
+	def __init__(self, **kwargs):
 		self.id = str(uuid.uuid4())
 		self._game = None
 		self.players = []
+		self.max_players = kwargs.get("max_players", 3)
 		self.clients = {}		# Player -> Handler
+		logging.info("created GameController for game "+self.id)
 	
 	def start_game(self):
-		pass
+		self._game = GameManager(self.players, GET_DECK(), RULES)
+		self._game.interface = WebSocketInterface
+		for _, client in self.clients.items():
+			self._game.observe(client)	# FIXME do real observer pattern, client.observe(observable)
+		logging.info("Starting game "+self.id)
+		game_thread = threading.Thread(target=self._game.run)
+		game_thread.start()
 	
 	def stop_game(self):
 		pass
---
+++ server/handlers/gamecreate.py
@@ -0,0 +1,23 @@
+"""
+"""
+import logging
+
+from server.controller import GameController
+
+from tornado.web import RequestHandler
+
+__all__ = ["GameCreateHandler"]
+
+
+class GameCreateHandler(RequestHandler):
+	"""Handler for creating games"""
+	def post(self):
+		"""
+		create a new GameController
+		"""
+		num_players = self.get_body_argument("number_players", 3)
+		logging.info("going to make a game with "+str(num_players)+" players")
+		controller = GameController(max_players=num_players)
+	
+	def check_xsrf_cookie(self):
+		return True # FIXME have a cleaner interface to this handler
