c5059be7b3ec97edafa57c071c53dbade6ecbc58
JoelPagliuca <joelpagliuca@hotmail.com>
2017-11-26 08:53:08 +1100

    constants file

---
+++ client/src/Constants.jsx
@@ -0,0 +1,22 @@
+export const CONSTANTS = {
+  CARD_WIDTH: 100,
+  CARD_HEIGHT: 150,
+};
+
+export const LOCATIONS = {
+  GAMEVIEW: "ws://"+window.location.host+"/websocket/gameview",
+  GAMECREATE: "ws://"+window.location.host+"/websocket/gamecreate"
+}
+
+export const ACTIONS = {
+  UPDATE: "UPDATE",
+  OPTION: "OPTION"
+}
+
+export const SUITS = {
+  YELLOW: '#FC0',
+  BLUE: '#05F',
+  RED: '#F00',
+  PURPLE: '#F0C',
+  BLACK: '#333'
+};
