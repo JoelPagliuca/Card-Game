abe0ac82432994037b52dbda08d7e6cf15fb9fbf
JoelPagliuca <joelpagliuca@hotmail.com>
2018-01-11 17:36:49 +1100

    indentation fix

--- client/src/Card.js
+++ client/src/Card.js
@@ -7,38 +7,38 @@ import { CARD_PROPS, SUITS } from "./Constants";
 
 export class Card extends Component {
 
-	render() {
-	  return (
-		<Grid item>
-		  <Stage width={CARD_PROPS.CARD_WIDTH+10} height={CARD_PROPS.CARD_HEIGHT+10}>
-			<Layer x={5} y={5}>
-			  <Rect
-				width={CARD_PROPS.CARD_WIDTH}
-				height={CARD_PROPS.CARD_HEIGHT}
-				fill={this.props.suit}
-				stroke={'#111'}
-				strokeWidth={2}
-				cornerRadius={2}
-			  />
-			  <Text
-				text={this.props.text.charAt(0)}
-				fontSize={CARD_PROPS.CARD_TEXT_SIZE}
-				fill={'#111'}
-				width={CARD_PROPS.CARD_WIDTH}
-				padding={20}
-				align={'center'}
-			  />
-			</Layer>
-		  </Stage>
-		</Grid>
-	  )
-	}
+  render() {
+    return (
+    <Grid item>
+      <Stage width={CARD_PROPS.CARD_WIDTH+10} height={CARD_PROPS.CARD_HEIGHT+10}>
+        <Layer x={5} y={5}>
+          <Rect
+            width={CARD_PROPS.CARD_WIDTH}
+            height={CARD_PROPS.CARD_HEIGHT}
+            fill={this.props.suit}
+            stroke={'#111'}
+            strokeWidth={2}
+            cornerRadius={2}
+          />
+          <Text
+            text={this.props.text.charAt(0)}
+            fontSize={CARD_PROPS.CARD_TEXT_SIZE}
+            fill={'#111'}
+            width={CARD_PROPS.CARD_WIDTH}
+            padding={20}
+            align={'center'}
+          />
+        </Layer>
+      </Stage>
+    </Grid>
+    )
   }
-  Card.propTypes = {
-	text: PropTypes.string,
-	suit: PropTypes.string
-  };
-  Card.defaultProps = {
-	text: '0', 
-	suit: SUITS.YELLOW
-  };
+}
+Card.propTypes = {
+  text: PropTypes.string,
+  suit: PropTypes.string
+};
+Card.defaultProps = {
+  text: '0', 
+  suit: SUITS.YELLOW
+};
